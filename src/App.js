fadd90ecf0cb13d542ab65f8c33a4ff91bf27a2c
Fernando Rauseo <fer200@Fernandos-MacBook-Pro.local>
2022-03-08 01:34:14 -0500

    first push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react'
+import { BrowserRouter as Router,Route } from 'react-router-dom';
+//import 'semantic-ui-css/semantic.min.css';
+//fix this ^^
+
+
 import './App.css';
+
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div><h1>Hello</h1></div>
+    //  <Router>
+    //    <Route exact path="/" component={Home} />
+    //  </Router>
+    //fix this too ^^
   );
 }
 
